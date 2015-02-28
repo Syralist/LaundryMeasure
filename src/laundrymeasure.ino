7c6ce6f87f36f2f2be982d16fda598dc2c530127
Thomas Helmke <thomas.helmke@gmx.de>
2015-02-28 17:39:03 +0100

    set up basic arduino functionality

---
+++ src/laundrymeasure.ino
@@ -0,0 +1,14 @@
+#include "TimerOne.h"
+
+void setup()
+{
+    pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(13, LOW);
+    delay(200);
+}
