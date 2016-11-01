3b83b9d8f9dda65a0d2e05fcfa925209ff1df3b8
Thomas Helmke <thomas.helmke@gmx.de>
2016-11-01 19:06:12 +0100

    added telegramrelais.php

---
+++ php/telegramrelais.php
@@ -0,0 +1,21 @@
+<?php
+header("Content-Type: text/html; charset=utf-8");
+
+define('BOT_TOKEN', $_GET["bot_token"]);
+define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+
+// grab the chatID
+$chatID = $_GET["chat_id"];
+
+// compose reply
+$reply =  sendMessage();
+
+// send reply
+$sendto = API_URL."sendmessage?chat_id=".$chatID."&text=".$reply;
+file_get_contents($sendto);
+
+function sendMessage(){
+    $message = urlencode($_GET["body"]);
+    return $message;
+}
+?>
