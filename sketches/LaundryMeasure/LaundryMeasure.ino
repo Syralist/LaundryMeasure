a87bf18e5ed9cc8b6f7d3b4797234a9906644a79
Thomas Helmke <thomas.helmke@gmx.de>
2016-11-19 16:38:47 +0100

    added OTA and did another testrun

--- sketches/LaundryMeasure/LaundryMeasure.ino
+++ sketches/LaundryMeasure/LaundryMeasure.ino
@@ -20,6 +20,7 @@
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
+#include <ArduinoOTA.h>
 
 #define MAX_BUFFER_SIZE 2500
 
@@ -280,6 +281,26 @@ void setup() {
     wifiManager.autoConnect("WaschmaschineAP");
     WiFiConnected = true;
 
+    ArduinoOTA.setHostname("WaschmaschineOTA");
+    ArduinoOTA.onStart([]() {
+            Serial.println("OTA Start");
+            });
+    ArduinoOTA.onEnd([]() {
+            Serial.println("\nOTA End");
+            });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+            Serial.printf("OTA Progress: %u%%\r", (progress / (total / 100)));
+            });
+    ArduinoOTA.onError([](ota_error_t error) {
+            Serial.printf("OTA Error[%u]: ", error);
+            if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+            else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+            else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+            else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+            else if (error == OTA_END_ERROR) Serial.println("End Failed");
+            });
+    ArduinoOTA.begin();
+
     broadcastIp = ~WiFi.subnetMask() | WiFi.gatewayIP();
 
     timeClient.begin();
@@ -289,6 +310,7 @@ void setup() {
 
 void loop() {
     // put your main code here, to run repeatedly:
+    ArduinoOTA.handle();
 
     if((millis()-lastUpdate)>updateInterval)
     {
